40c069087b3bd83ca1f849f067d8f8416cfee950
pokleh <mfauzi@gmail.com>
2025-12-26 16:34:26 +0800

    cleanup(navigation): remove legacy AdminLTE navbar code
    - Remove leftover AdminLTE navbar with pushmenu toggle and home link
    - Clean up redundant navigation elements that are no longer needed
    - Remove AdminLTE navbar structure that was conflicting with Tabler layout
    - Eliminate duplicate navigation elements behind sidebar
    - Maintain clean Tabler-only navigation structure
    - Remove data-widget pushmenu functionality (replaced by Tabler navbar)
    
    Fixes navigation clutter by removing obsolete AdminLTE navbar elements

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -508,43 +508,6 @@
 <body>
     <div class="layout-fluid">
 
-        <!-- Sidebar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="<?= base_url('/dashboard') ?>" class="nav-link">Home</a>
-                </li>
-            </ul>
-
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#" data-widget="fullscreen" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="fas fa-user"></i>
-                        <span class="d-none d-md-inline"><?= session()->get('name') ?? 'Admin' ?></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-user mr-2"></i> Profile
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="<?= base_url('/auth/logout') ?>" class="dropdown-item">
-                            <i class="fas fa-sign-out-alt mr-2"></i> Logout
-                        </a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
         <aside class="navbar navbar-vertical navbar-expand-lg navbar-dark">
